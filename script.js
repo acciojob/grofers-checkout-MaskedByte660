81aff1d262127a58b812668cf7f23ae943b389e8
MaskedByte660 <codingtechinfo01@gmail.com>
2025-02-14 12:46:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	//to get total value
+	let prices = document.querySelectorAll(".price");
+	let sum =0;
+
+	prices.forEach((price)=>{
+		sum = sum + parseInt(price.textContent)
+	})
+
+	
+	let newRowExist  = document.getElementById("ans");
+	
+	
+	if(!newRowExist){
+		let newRow = document.createElement("tr");
+		newRow.id ="ans";
+		let newCell = document.createElement("td");
+		newCell.textContent =`Total Price = ${sum}`
+		newRow.append(newCell);
+		document.querySelector("table").append(newRow);
+	}
+	
+	//create a row and add the total price
   
 };
 
